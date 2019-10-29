1ad563132cdb8e624c71899a5fc3429e356c76d5
Daniel Mendes <daniel.mendes@totvs.com.br>
2019-10-29 16:26:26 -0300

    Continuação do curso.

---
+++ src/array01.py
@@ -0,0 +1,14 @@
+import array as arr
+import numpy as np
+
+numArray = arr.array('d', [1, 3.5])
+
+print(numArray)
+
+numeros = np.array([1, 3.5])
+
+print(numeros)
+
+numeros = numeros + 3
+
+print(numeros)
